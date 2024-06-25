aae18e730b78cf15b0a34e58067f7da1a44b58f2
reversefall <nphutien.1704@gmail.com>
2024-06-25 07:26:09 +0700

    Add project files.

---
+++ ASM/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ASM.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var JobSeeker = new IdentityRole("JobSeeker");
+            JobSeeker.NormalizedName = "JobSeeker";
+
+            var Employer = new IdentityRole("Employer");
+            Employer.NormalizedName = "Employer";
+
+            builder.Entity<IdentityRole>().HasData(admin, JobSeeker,Employer);
+
+
+        }
+    }
+}
